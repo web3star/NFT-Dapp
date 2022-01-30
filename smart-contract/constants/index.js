bedff345ee9ea7a99f00bd12b495e4c6fd19c95b
Sunthosh <heysunthosh@gmail.com>
2022-01-30 14:48:43 +0530

    Added NFT Dapp code

---
+++ smart-contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6030D677823BE46cD4cc0eE7DaC612AE620630d8";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
