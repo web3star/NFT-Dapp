84d395ca3e51f2881744591a9eeaa20d8a4e09cd
Sunthosh <heysunthosh@gmail.com>
2022-01-30 17:10:25 +0530

    fixed the metadata uri

--- smart-contract/constants/index.js
+++ smart-contract/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6030D677823BE46cD4cc0eE7DaC612AE620630d8";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-dapp-psi.vercel.app/api/";
+const METADATA_URL = "https://nft-dapp-three.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
